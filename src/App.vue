40bb1d999f8cba09417a31dd5574161857ad91c6
parker lindley <fugazi8@gmail.com>
2018-05-03 12:46:45 -0600

    Add header and add basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+body, html {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="head">
+    <h1 class="title">TRAIL FIND</h1>
+  </header>
+</template>
+<script>
+</script>
+
+<style scoped>
+  
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('../assets/mountain.jpg');
+    background-size: cover;
+    height: 300px;
+  }
+
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 25%;
+    margin: 0;
+    color: #fff;
+    background: rgba(0, 0, 0, 0.4);
+  }
+
+</style>
