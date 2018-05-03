14638e11902d35dddd135fd219d3c73830191e68
parker lindley <fugazi8@gmail.com>
2018-05-03 12:57:42 -0600

    Add main component input and default input

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <Header />
+    <Main />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Main from './components/Main'
+
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+
+    <form>
+      <label>Difficulty</label>
+      <select>
+        <option>Easy</option>
+        <option>Beginner</option>
+        <option>Intermediate</option>
+        <option>Advanced</option>
+        <option>Expert</option>
+      </select>
+      <label>Distance(in miles)</label>
+      <input type='number' min="1" max="150">
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data: () => ({
+    difficulty: 'beginner',
+    distance: 25
+  })
+}
+</script>
+
+<style>
+
+</style>
+
+
