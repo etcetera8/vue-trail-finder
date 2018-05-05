5d992eccb084d297af3503eb9b04201e1fb2bdaa
parker lindley <fugazi8@gmail.com>
2018-05-05 11:43:37 -0600

    Create trailcard element

---
+++ src/components/TrailCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <img src="#" alt="image here">
+    <h1>Title</h1>
+    <span>Difficulty</span>
+    <span>Trail Rating</span>
+    <p>Description</p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+  .card {
+    border: 1px solid gray;
+  }
+</style>
+
